05388743d9feec90c1611bd1b4471c8460057905
Andres <andrescasa.dev@gmail.com>
2022-10-13 12:09:37 -0500

    responsive nav

--- Jake&Elwood/app.js
+++ Jake&Elwood/app.js
@@ -2,6 +2,9 @@
 document.addEventListener('click', (event)=>{
   const {target} = event;
   if(target.matches('.hamburger')){
-    document.querySelector('.nav-content').classList.toggle('open')
+    document.querySelector('.nav').classList.add('open')
+  }
+  if(target.matches('.nav__close')){
+    document.querySelector('.nav').classList.remove('open')
   }
 })
